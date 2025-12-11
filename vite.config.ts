484608fa0dc24d0ccd653a92a37b14d552e2d601
Sushant Telrandhe <105573276+0jokerkiller0@users.noreply.github.com>
2025-12-11 13:10:51 +0530

    Update base path for GitHub Pages
    Set base path for GitHub Pages deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/My-Portfolio-Sushant-Telrandhe/",  // REQUIRED FOR GITHUB PAGES
+
   server: {
     host: "::",
     port: 8080,
   },
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
