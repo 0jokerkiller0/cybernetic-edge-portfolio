7b9925b2c2749441ecd78c0c0ff80bd04acc8ee4
Sushant Telrandhe <105573276+0jokerkiller0@users.noreply.github.com>
2025-12-11 13:04:33 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,14 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/My-Portfolio-Sushant-Telrandhe/", // REQUIRED for GitHub Pages
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/My-Portfolio-Sushant-Telrandhe/",
+
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "src").replace(/\\/g, "/"),
     },
   },
 }));
